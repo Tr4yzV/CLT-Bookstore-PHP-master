790e0d85baf68806bbc033358b584b1471d57078
Tr4yzV <trayzv.ph@gmail.com>
2025-04-20 22:34:39 +0700

    Update sign.php

--- sign.php
+++ sign.php
@@ -13,7 +13,10 @@
     <link rel="icon" href="/assets/img/favicon.png" />
 </head>
 
-<?php
+<?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // Đăng nhập
         if (isset($_POST['SignIn'])) {
