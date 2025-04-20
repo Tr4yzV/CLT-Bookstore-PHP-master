4d4d00517b5b7257d39681f3cb951085937b1011
Tr4yzV <trayzv.ph@gmail.com>
2025-04-20 22:48:06 +0700

    Update sign.php

--- sign.php
+++ sign.php
@@ -1,5 +1,3 @@
-<?php include './config/config.php';?>
-<?php include './config/database.php';?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,10 +11,13 @@
     <link rel="icon" href="/assets/img/favicon.png" />
 </head>
 
-<?php 
+<?php
+include './config/config.php';
+include './config/database.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+session_start();
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // Đăng nhập
         if (isset($_POST['SignIn'])) {
@@ -26,7 +27,6 @@ error_reporting(E_ALL);
             $sql = "SELECT * FROM users WHERE (Username = '$username' OR Phone = '$username' OR Email = '$username') AND Password = sha1('$password') AND Status = 1";
             $users = Database::GetData($sql);
             if ($users != null) {
-                session_start();
                 $user = $users[0];
                 $_SESSION['Username'] = $user['Username'];
                 $_SESSION['DisplayName'] = $user['Fullname'] == '' ? $user['Username'] : $user['Fullname'];
