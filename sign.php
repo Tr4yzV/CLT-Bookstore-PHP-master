38460adf2cd3555e4a6bdc4672ea358dd0e4580c
Tr4yzV <trayzv.ph@gmail.com>
2025-04-20 22:55:26 +0700

    Update sign.php

--- sign.php
+++ sign.php
@@ -1,15 +1,3 @@
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./assets/css/sign.css" />
-    <title>Đăng ký và đăng nhập</title>
-    <link rel="icon" href="/assets/img/favicon.png" />
-</head>
 <?php
 include './config/config.php';
 include './config/database.php';
@@ -67,6 +55,17 @@ session_start();
         }
     }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./assets/css/sign.css" />
+    <title>Đăng ký và đăng nhập</title>
+    <link rel="icon" href="/assets/img/favicon.png" />
+</head>
 
 <body>
     <div class="container">
